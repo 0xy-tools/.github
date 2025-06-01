a93d8a20f9fcb35f9f45985052901c8aaf8b0da1
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-06-01 14:23:56 +0200

    feat: adding basic style and color palette

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/root.css">
+    <!-- <link rel="stylesheet" href="./styles/common.css">
+    <link rel="stylesheet" href="./styles/menu.css">
+    <script src="./lib/qrcode.min.js"></script> -->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="viewport"
+        content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
+    <title>0xy</title>
+
+    <meta name="author" content="MagicTINTIN">
+    <meta name="description" content="CPOI is a service/browser extension that allows you to copy and paste text between your different devices.\nSimple and fast to use, no sign-up or account required.">
+
+    <link rel="icon" type="image/x-icon" href="images/favicon.png">
+
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="CPOI">
+    <meta property="og:description" content="CPOI is a service/browser extension that allows you to copy and paste text between your different devices.\nSimple and fast to use, no sign-up or account required.">
+
+    <meta property="og:image" content="https://0xy.fr/images/favicon.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:alt" content="Logo of CPOI">
+
+    <meta property="og:url" content="https://0xy.fr/" />
+    <meta data-react-helmet="true" name="theme-color" content="#00f0ff" />
+
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "name": "0xy",
+            "alternateName": ["oxy","Oxy"],
+            "url": "https://0xy.fr/",
+            "inLanguage": "en",
+            "isAccessibleForFree": "true",
+            "isFamilyFriendly": "true",
+            "isPartOf": "https://0xy.fr/",
+            "keywords": "0xy, Oxy",
+            "description": "0xy is an utilities application suite",
+            "alternateName": "Copy Paste Over Internet"
+        }
+    </script>
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "name": "0xy",
+            "alternateName": ["oxy","Oxy"],
+            "url": "https://0xy.fr/",
+            "inLanguage": "fr",
+            "isAccessibleForFree": "true",
+            "isFamilyFriendly": "true",
+            "isPartOf": "https://0xy.fr/",
+            "keywords": "copier, coller, en ligne, appareils",
+            "description": "0xy est une suite logicielle d'utilitaires",
+            "alternateName": "Copy Paste Over Internet"
+        }
+    </script>
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "SoftwareApplication",
+            "name": "0xy",
+            "applicationSuite": "0xy",
+            "applicationCategory": "UtilitiesApplication",
+            "featureList": "0xy is an utilities application suite",
+            "url": "https://0xy.fr/",
+            "offers": {
+                "@type": "Offer",
+                "price": 0
+            }
+        }
+    </script>
+</head>
+
+<body class="bg">
+</body>
+
+</html>
